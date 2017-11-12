b979387481c5efba333935971c88e23e74a89724
Lavenderjiang <lavenderchiang@gmail.com>
2017-11-12 12:04:11 -0500

    final submission web

---
+++ webApp/instaclone/instaclone/static/js/index.js
@@ -0,0 +1,81 @@
+// This example uses MediaRecorder to record from a live audio stream,
+// and uses the resulting blob as a source for an audio element.
+//
+// The relevant functions in use are:
+//
+// navigator.mediaDevices.getUserMedia -> to get audio stream from microphone
+// MediaRecorder (constructor) -> create MediaRecorder instance for a stream
+// MediaRecorder.ondataavailable -> event to listen to when the recording is ready
+// MediaRecorder.start -> start recording
+// MediaRecorder.stop -> stop recording (this will generate a blob of data)
+// URL.createObjectURL -> to create a URL from a blob, which we can use as audio src
+
+var recordButton, stopButton, recorder;
+
+
+/*for (var i = 0; i < 20; i++) {
+  var x = $('li')[i];
+  $(x).css('-webkit-animation', 'music 1s ' + i + '00ms  ease-in-out both infinite');
+}*/
+function dancingNotes(){
+  for (var i = 0; i < 20; i++) {
+    var x = $('li')[i];
+    $(x).css('-webkit-animation', 'music 1s ' + i + '00ms  ease-in-out both infinite');
+  }
+}
+
+
+
+window.onload = function () {
+
+  recordButton = document.getElementById('record');
+  stopButton = document.getElementById('stop');
+
+  // get audio stream from user's mic
+  navigator.mediaDevices.getUserMedia({
+    audio: true
+  })
+  .then(function (stream) {
+    recordButton.disabled = false;
+    recordButton.addEventListener('click', startRecording);
+    stopButton.addEventListener('click', stopRecording);
+    recorder = new MediaRecorder(stream);
+
+    // listen to dataavailable, which gets triggered whenever we have
+    // an audio blob available
+    recorder.addEventListener('dataavailable', onRecordingReady);
+  });
+};
+
+function startRecording() {
+  recordButton.disabled = true;
+  stopButton.disabled = false;
+  dancingNotes();
+  recorder.start();
+}
+
+function stopRecording() {
+  recordButton.disabled = false;
+  stopButton.disabled = true;
+
+  // Stopping the recorder will eventually trigger the `dataavailable` event and we can complete the recording process
+  recorder.stop();
+}
+
+function onRecordingReady(e) {
+  var audio = document.getElementById('audio');
+  // e.data contains a blob representing the recording
+  audio.src = URL.createObjectURL(e.data);
+  audio.play();
+
+  //Download the blob
+  //Code from https://mozdevs.github.io/MediaRecorder-examples/download-blob.html
+  var link = document.createElement('a');
+  link.setAttribute('href', URL.createObjectURL(e.data));
+  link.setAttribute('download', 'audio.txt');
+
+  // NOTE: We need to add temporarily the link to the DOM so
+  //       we can trigger a 'click' on it.
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);}
